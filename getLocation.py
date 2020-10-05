6254af6e41cf3e1872ddf680f1252298cb7d9397
Rich Hastings <user@richdev.Home>
2020-10-05 13:04:19 -0700

    Added commented out linesto use another API if needed

--- getLocation.py
+++ getLocation.py
@@ -1,8 +1,11 @@
 from geopy.geocoders import GoogleV3
+#from geopy.geocoders import Noninatim
 
 def getLocation(lat, lon):
-    location=str(lat) + "," + " "+str(lon)
+    """helper function to get address from lat and lon"""
+    # geolocator = Nominatim(user_agent="project1") #remove comment for Nominatim
+    location = str(lat) + "," + " "+str(lon)
     geolocator = GoogleV3(api_key="AIzaSyDbc-uvarJSL3JSaNkyO1lCDQoawBpRdhM") #assigns key for search
-    location = geolocator.reverse((location))
+    location = geolocator.reverse(location)
     return location.address
 
