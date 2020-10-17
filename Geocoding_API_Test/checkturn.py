041e91a80297b6b46371cf5738b71c8fd7335cb3
Rich-Hastings <guy.hastings@gmail.com>
2020-10-17 10:17:49 -0700

    updated name of file for turns

---
+++ Geocoding_API_Test/checkturn.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def checkDirection(p1, p2, p3):
+    """Helper function that will check the direction of a turn
+    takes 3 points in np arrays and returns string of direction turned or 1 if no turn was made"""
+    a = p2-p1
+    b = p3-p2
+    turn= a[0]*b[1]-a[1]*b[0]
+    if turn > 0:
+        return "right"
+    elif turn < 0:
+        return "left"
+    else:
+        uturn = a[0]*b[0]+a[1]*b[1]
+        if uturn > 0:
+            return "straight"
+        elif uturn < 0:
+            return "U-Turn"
+        else:
+            return "No Movement"
