b0bf90a305c022bab49d09eaab8797b4e378660d
Rich-Hastings <72363497+Rich-Hastings@users.noreply.github.com>
2020-10-23 13:37:57 -0700

    Update checkturn.py
    Cleaned get location and check turn.

--- Geocoding_API_Test/checkturn.py
+++ Geocoding_API_Test/checkturn.py
@@ -11,20 +11,15 @@ from functools import partial
 def getLocation(lat, lon):
     """ helper function to get address from lat and lon
 
-        inputs: list of latitude numbers, list of longitude numbers,
-        an api key
+        inputs: list of latitude numbers, list of longitude numbers returns string for address
     """
 
-    geolocator = Nominatim(user_agent="project1")  # remove comment for Nominatim
+    geolocator = Nominatim(user_agent="project1")  # assign nominating as your geolocator
     location = (lat, lon)
     reverse = RateLimiter(partial(geolocator.reverse, addressdetails=False, zoom=17), max_retries=5, min_delay_seconds=1)
-    # geolocator = GoogleV3(my_api_key) #assigns key for search
+    
     location=reverse(location)
-    # geolocator = GoogleV3(my_api_key) #assigns key for search
-    #try:
-        #except (GeocoderUnavailable, GeocoderTimedOut):
-    #print("geocoder is not accepting requests,please try again later")
-
+    
     if (location != None):
         return location.address
 
@@ -34,15 +29,11 @@ def getLocation(lat, lon):
 
 
 def checkDirection(p1, p2, p3):
-    """Helper function that will check the direction of a turn
-    takes 3 points in np arrays and returns string of direction turned or 1 if no turn was made"""
-    #print(p1)
-    #print(p2)
-    #print(p3)
+    """Helper function that will check the direction of a turn using array cross multiplication
+    takes 3 points in np arrays and returns string of direction turned or 0 if no turn was made"""
     a = p2-p1
     b = p3-p2
-    #print (a)
-    #print (b)
+    
     turn= a[0]*b[1]-a[1]*b[0]
     if turn > 0:
         return "right"
@@ -50,13 +41,7 @@ def checkDirection(p1, p2, p3):
         return "left"
     else:
         return 0
-        """uturn = a[0]*b[0]+a[1]*b[1]
-        if uturn > 0:
-            return "straight"
-        elif uturn < 0:
-            return "U-Turn"
-        else:
-            return "No Movement"""
+        
 
 def getdirections(LatitudeList, LongitudeList, listSize):
     """
