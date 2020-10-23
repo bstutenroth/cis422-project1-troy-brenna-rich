0ea64b5e605ccc83b2a7c09fe4234a5a45651a75
Rich Hastings <user@richdev.Home>
2020-10-23 14:52:14 -0700

    Cleaned code

--- Geocoding_API_Test/checkturn.py
+++ Geocoding_API_Test/checkturn.py
@@ -16,13 +16,14 @@ def getLocation(lat, lon):
     """ helper function to get address from lat and lon
 
         inputs: list of latitude numbers, list of longitude numbers returns string for address
+        returns string with the address
     """
 
     geolocator = Nominatim(user_agent="project1")  # assign nominating as your geolocator
     location = (lat, lon)
     reverse = RateLimiter(partial(geolocator.reverse, addressdetails=False, zoom=17), max_retries=5, min_delay_seconds=1)
     
-    location=reverse(location)
+    location = reverse(location)
     
     if (location != None):
         return location.address
@@ -34,12 +35,14 @@ def getLocation(lat, lon):
 
 def checkDirection(p1, p2, p3):
 
-    """Helper function that will check the direction of a turn using array cross multiplication
-    takes 3 points in np arrays and returns string of direction turned or 0 if no turn was made"""
-    
-    #print(p1)
-    #print(p2)
-    #print(p3)
+    """ WARNING this will not test for uturns 
+    Helper function that will check the direction of a turn using array cross multiplication
+    takes 3 points in np arrays and returns string of direction turned or 0 if no turn was made
+
+
+    future version will have a turn tolerence to detect uturn and reduce calls to service and test for uturns
+    """
+
     a = p2-p1
     b = p3-p2
     
@@ -57,46 +60,44 @@ def getdirections(LatitudeList, LongitudeList, listSize):
     """
     Takes a list of latitudes, longitudes, thesizeof the list and a given api key.
     Creates a list of streets, direction turned on to street and distance traveled on street
+    Returns list
     """
     
     logging.basicConfig()
     logger = logging.getLogger("geopy")
-    quesheet = [[]]  # list of lists containing, street, turn direction, distance
+    quesheet = [[]]  # list of lists containing, street, turn direction, distance, then initialize with first location
     start = getLocation(LatitudeList[0], LongitudeList[0])
-    start = start.split(",")
+    start = start.split(",")  # will be inserted in to get location for modularity
     quesheet[0].append(start[0])
     quesheet[0].append("start")
     quesheet[0].append(
         distance.distance((LatitudeList[0], LongitudeList[0]), (LatitudeList[1], LongitudeList[1])).miles)
-    queueplace = 0
+    queueplace = 0  # location in list that most recent turn was made
     oldPercentage = 0
     for i in range(2, listSize - 2):
         percentage = (i/listSize) * 100
         if (percentage != oldPercentage):
-            print("{} percent complete.".format(str(round(percentage, 2))), end='\r')
+            #print("{} percent complete.".format(str(round(percentage, 2))), end='\r')
             oldPercentage = percentage
 
         test = checkDirection(np.array([LatitudeList[i - 1], LongitudeList[i - 1]]),
                               np.array([LatitudeList[i], LongitudeList[i]]),
                               np.array([LatitudeList[i + 1], LongitudeList[i + 1]]))
-        # print(i)
-        if test != 0:
-            # print(test)
+
+        if test:
             streetCheck = getLocation(LatitudeList[i + 1], LongitudeList[i + 1])
             streetCheck = streetCheck.split(",")
             streetCheck = streetCheck[0]
-            # print(streetCheck)
-            if streetCheck == quesheet[queueplace][0]:
+
+            if streetCheck == quesheet[queueplace][0]:  # check if there was a turn or winding street.
                 quesheet[queueplace][2] += distance.distance((LatitudeList[i - 1], LongitudeList[i - 1]),
                                                              (LatitudeList[i], LongitudeList[i])).miles
             else:
-                # print("new street")
                 streetCheckNext = getLocation(LatitudeList[i], LongitudeList[i])
                 streetCheckNext = streetCheck.split(",")
                 streetCheckNext = streetCheckNext[0]
                 if streetCheck == streetCheckNext:
                     queueplace += 1
-                    # print(queueplace)
                     quesheet.append([])
                     quesheet[queueplace].append(streetCheck)
                     quesheet[queueplace].append(test)
@@ -105,9 +106,6 @@ def getdirections(LatitudeList, LongitudeList, listSize):
         else:
             quesheet[queueplace][2] += distance.distance((LatitudeList[i - 1], LongitudeList[i - 1]),
                                                          (LatitudeList[i], LongitudeList[i])).miles
-        # print(getLocation(LatitudeList[i], LongitudeList[i], my_api_key))
-        # print (test)
-
     return quesheet
 
 '''
