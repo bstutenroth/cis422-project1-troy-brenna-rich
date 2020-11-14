6bb5c1b8b0525939495dc7093a99431e1a5c1fca
Brenna Stutenroth <brennastutenroth@gmail.com>
2020-11-13 18:29:41 -0800

    basic website works, and user login/register implemented

--- Geocoding_API_Test/main_flask.py
+++ Geocoding_API_Test/main_flask.py
@@ -2,22 +2,42 @@
 # Brenna Stutenroth
 
 import os
-import flask
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template, request, jsonify, session
+from pymongo import MongoClient
 import urllib
-import geopy
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+import json
 from werkzeug.utils import secure_filename
 from reverse_geocoding_test import *
+import logging
+from bson.json_util import dumps
+import bcrypt
+import config
 
 UPLOAD_FOLDER = ''
 ALLOWED_EXTENSIONS = {'gpx'}
 
 app = Flask(__name__)
+CONFIG = config.configuration()
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'some secret key'
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
+mongo = client.db
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'username' in session:
+        return render_template('index.html')
+    else:
+        return render_template('landing.html')
+
+@app.route('/get_route')
+def get_route():
+    return render_template('get_route.html')
+
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
 
 @app.route('/display')
 def display():
@@ -27,7 +47,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/get_route', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -45,3 +65,43 @@ def upload_file():
             filename = secure_filename(file.filename)
             return render_template('display.html', adresses = main(filename))
     return
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'], login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('login'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('login'))
+
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+@app.route('/logout', methods = ['GET'])
+def logout():
+    session.pop('username', None)
+    return render_template('landing.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+if app.debug:
+    app.logger.setLevel(logging.DEBUG)
