a2dfde4628d34a95a827e2fc5c4cc333a424f70a
tclendo <33107696+tclendo@users.noreply.github.com>
2020-11-24 17:25:51 -0800

    Revert "Delete api_keys.py"
    This reverts commit 38701d6eec0699f67e603ba11e979755a3207d21.

---
+++ Geocoding_API_Test/api_keys.py
@@ -0,0 +1,6 @@
+# api_keys.py
+# Troy Clendenen
+
+maps_api_key = "5b3ce3597851110001cf62487f8d937af0ff4d2cb7e98d75b7aca2b6"
+aqi_api_key = "bda0064a3c3e7c8b65e8acc261ad0bf060adb036"
+weather_api_key = "128c79adf7c5d764e4ee5407bbf08de5" #weather api key
